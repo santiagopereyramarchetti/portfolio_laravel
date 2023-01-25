bd73273e88e423a984d5c4f63821e44e256425be
Santiago Pereyra <santi@MSI.localdomain>
2023-01-25 01:42:48 -0300

    Routes ready. Index and create controller method and basic page ready

---
+++ resources/js/Pages/Project/Create.vue
@@ -0,0 +1,24 @@
+<template>
+  <Head title="New Project" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Create Project</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                Form
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import { Head } from '@inertiajs/vue3';
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/Pages/Skills/Create.vue
@@ -0,0 +1,27 @@
+<template>
+  <Head title="New Skill" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">New Skill</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex justify-end m-2 p-2">
+                   Form
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import { Head } from '@inertiajs/vue3';
+    import { Link } from '@inertiajs/vue3'
+</script>
+
+<style>
+
+</style>
