880631bae65faa4131e75f5cdb0613dd871dad11
Santiago Pereyra <santi@MSI.localdomain>
2023-01-26 18:20:20 -0300

    Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,37 @@ module.exports = {
     ],
 
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
+        fontFamily:{
+            primary: "Playfair Display",
+            body: "work Sans"
         },
+        container:{
+            padding:{
+                DEFAULT: "1rem",
+                lg: "3rem"
+            }
+        },
+        extend: {
+            colors:{
+                "light-primary": "#FAF1E6",
+                "light-secondary-primary": "#DDFAF6",
+                "light-tail-100": "#A0DED2",
+                "light-tail-500": "#54BAB9",
+                "dark-primary": "#050402",
+                "dark-secondary-primary": "#1C1D24",
+                "dark-tail-100": "#07567D",
+                "dark-tail-500": "#292D42",
+                accent: {
+                    DEFAULT: "#AC6B34",
+                    hover: "#925A2B"
+                },
+                paragraph: "#878E99",
+                blue: {...defaultTheme.colors.blue},
+                indigo: {...defaultTheme.colors.indigo},
+                green: {...defaultTheme.colors.green},
+                red: {...defaultTheme.colors.red}
+            }
+        }
     },
 
     plugins: [require('@tailwindcss/forms')],
